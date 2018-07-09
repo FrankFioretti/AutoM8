fa841b2e6a1aad583030691d6bcb2ae9841a88f2
FrankFioretti <40969570+FrankFioretti@users.noreply.github.com>
2018-07-09 09:23:26 -0500

    DHCP

---
+++ DHCP_2_Static.ps1
@@ -0,0 +1,6 @@
+﻿$DG = ((Get-NetIPConfiguration).IPv4DefaultGateway).NextHop
+$IP = ((Get-NetIPConfiguration).IPv4Address).IPAddress
+$DNS = Get-DnsClientServerAddress -InterfaceAlias "Ethernet" | Select Object -ExpandProperty ServerAddresses
+Set-NetIPInterface -InterfaceAlias "Ethernet" -Dhcp Disabled
+New-NetIPAddress -InterfaceAlias "Ethernet" -IPAddress $IP -PrefixLength 24 -DefaultGateway $DG
+Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses $DNS
