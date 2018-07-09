c412fac78a506bf4874d8e141f2a01f62d203c76
FrankFioretti <40969570+FrankFioretti@users.noreply.github.com>
2018-07-09 14:57:23 -0500

    Scripts
    Creates a new S3 Bucket, IAM user, and IAM policy.

---
+++ New_S3_&_IAM_User.ps1
@@ -0,0 +1,71 @@
+﻿$clientname = "TestClient"
+$bucketname = "test-bucket"
+$region = us-west-2
+
+$json = '{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListBucketByTags",
+                "s3:GetLifecycleConfiguration",
+                "s3:GetBucketTagging",
+                "s3:GetInventoryConfiguration",
+                "s3:GetObjectVersionTagging",
+                "s3:ListBucketVersions",
+                "s3:GetBucketLogging",
+                "s3:ListBucket",
+                "s3:GetAccelerateConfiguration",
+                "s3:GetBucketPolicy",
+                "s3:GetObjectVersionTorrent",
+                "s3:GetObjectAcl",
+                "s3:GetBucketRequestPayment",
+                "s3:GetObjectVersionAcl",
+                "s3:GetObjectTagging",
+                "s3:GetMetricsConfiguration",
+                "s3:DeleteObject",
+                "s3:GetIpConfiguration",
+                "s3:ListBucketMultipartUploads",
+                "s3:GetBucketWebsite",
+                "s3:GetBucketVersioning",
+                "s3:GetBucketAcl",
+                "s3:GetBucketNotification",
+                "s3:GetReplicationConfiguration",
+                "s3:ListMultipartUploadParts",
+                "s3:PutObject",
+                "s3:GetObject",
+                "s3:GetObjectTorrent",
+                "s3:GetBucketCORS",
+                "s3:GetAnalyticsConfiguration",
+                "s3:GetObjectVersionForReplication",
+                "s3:GetBucketLocation",
+                "s3:GetObjectVersion"
+            ],
+            "Resource": [
+                "arn:aws:s3:::$bucketname/*",
+                "arn:aws:s3:::$bucketname"
+            ]
+        },
+        {
+            "Sid": "VisualEditor1",
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListAllMyBuckets",
+                "s3:HeadBucket",
+                "s3:ListObjects"
+            ],
+            "Resource": "*"
+        }
+    ]
+}' 
+
+$json = $ExecutionContext.InvokeCommand.ExpandString($json)
+
+$arn = (New-IAMPolicy -PolicyName $clientname -PolicyDocument $json).Arn
+New-S3Bucket -Bucketname $bucketname -Region $region
+Write-S3BucketVersioning -BucketName $bucketname -VersioningConfig_Status Enabled
+New-IAMUser -UserName $clientname
+$keys = New-IAMAccessKey -UserName $clientname
+Register-IAMUserPolicy -UserName $clientname -PolicyArn $arn
