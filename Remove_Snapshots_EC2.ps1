0a1dc747537359b49fffe4098025bbc1139127cc
FrankFioretti <40969570+FrankFioretti@users.noreply.github.com>
2018-07-09 08:55:00 -0500

    Scripts
    Scripts

---
+++ Remove_Snapshots_EC2.ps1
@@ -0,0 +1,21 @@
+﻿## Delete Old Snapshots ##
+
+$region = us-west-2
+$instances = ((get-ec2instance -filter @( @{name='tag:Backup';values="Auto"}) -Region $region).instances).instanceid
+$volumes =foreach ($instance in $instances) {
+(((Get-EC2InstanceAttribute -instanceid $instance -region $region -Attribute blockdevicemapping).blockdevicemappings).ebs).volumeid
+}
+
+foreach ($volume in $volumes) {
+    $snapshots = Get-EC2Snapshot -filter  @( @{name='volume-id';values="$volume"}) -region $region
+        foreach ($snapshot in $snapshots) {
+        $snapID = ($snapshot).SnapshotID
+        $SnapTime = ($snapshot).Starttime
+        $currentdate = (get-date)
+        $snapage = (New-timespan -Start $currentdate -End $snaptime).days
+        IF($snapage -ge "30") {
+        Remove-Ec2Snapshot -SnapshotId $snapid -region $region -force
+        } Else {
+        }
+        }
+    }
---
+++ Snapshot_EC2.ps1
@@ -0,0 +1,11 @@
+﻿## Create New Snapshots ##
+
+$region = us-west-2
+$instances = ((get-ec2instance -filter @( @{name='tag:Backup';values="Auto"}) -Region $region).instances).instanceid
+$volumes =foreach ($instance in $instances) {
+(((Get-EC2InstanceAttribute -instanceid $instance -region $region -Attribute blockdevicemapping).blockdevicemappings).ebs).volumeid
+}
+foreach ($volume in $volumes) {
+$attachedinstance = ((get-ec2volume -VolumeId $volume -region $region).attachments).instanceid
+New-ec2snapshot -VolumeId $volume -Description "Automatic Snapshot for $AttachedInstance" -region $region
+}
