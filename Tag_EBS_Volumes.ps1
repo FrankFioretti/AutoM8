a4b61ba5c79182f9ea26ea3580e748030a744b63
FrankFioretti <40969570+FrankFioretti@users.noreply.github.com>
2018-07-12 21:09:10 -0500

    Scripts
    Searches out EC2 instances with a given tag (Backup, Auto) and applies it to the attached EBS volumes. Can be used with DLM (where available).

---
+++ Tag_EBS_Volumes.ps1
@@ -0,0 +1,11 @@
+﻿$region = "us-west-2"
+$instances = ((get-ec2instance -filter @( @{name='tag:Backup'; values="Auto"}; @{name='instance-state-code'; values = 0,80,16,32,64} ) -Region $region).instances).instanceid
+$volumes =foreach ($instance in $instances) {
+(((Get-EC2InstanceAttribute -instanceid $instance -region $region -Attribute blockdevicemapping).blockdevicemappings).ebs).volumeid
+}
+foreach ($volume in $volumes) {
+$tag = New-Object Amazon.EC2.Model.Tag
+$tag.Key = "Backup"
+$tag.Value = "Auto"
+New-EC2Tag -Resources $volume -Tags $tag -region $region
+}
